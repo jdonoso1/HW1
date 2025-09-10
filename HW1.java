25932bec6a85c9612acdd8587dcf594e5e1adc1e
jdonoso1 <151100870+jdonoso1@users.noreply.github.com>
2025-09-10 13:59:12 -0500

    HW1.java

--- HW1.java
+++ HW1.java
@@ -87,162 +87,216 @@ public class HW1 {
          */
         public void removeElementsLT ( int ltValue ) {
 
-        while (head != null & head.data < ltValue) {
-            head = head.next;
-
-            return;
-        }
-
-
-        /*
-         * Method removeElement() - this method removes all nodes that contain a
-         * value equal to the value the provided parameter 'value'.
-         */
-
-        public void removeElement ( int value ) {
-
-            // YOUR CODE GOES HERE
-
-            return;
-        }
-
-
-        /*
-         * Method toString() - this is a helper method for printing / constructing
-         * a string object from the linked-list.
-         */
-        public String toString () // Method to output the LinkedList as a String
-        {
-            String output = "[";
-            Node currNode = this.head;
-            while (currNode != null) {
-                output += currNode.data + " ";
-                currNode = currNode.next;
+            while (head != null && head.data < ltValue) {
+                head = head.next;
             }
-            return output.trim() + "]";
+
+
+            Node current = head;
+            while (current != null && current.next != null) {
+                if (current.next.data < ltValue) {
+                    current.next = current.next.next;
+                } else {
+                    current = current.next;
+                }
+            }
+            return;
         }
 
-    } // End class LinkedList
+
+
+            /*
+             * Method removeElement() - this method removes all nodes that contain a
+             * value equal to the value the provided parameter 'value'.
+             */
+
+            public void removeElement( int value ){
+
+                while (head != null && head.data == value) {
+                    head = head.next;
+                }
+                Node current = head;
+                while (current != null && current.next != null) {
+                    if (current.next.data == value) {
+                        current.next = current.next.next;
+                    } else {
+                        current = current.next;
+                    }
+                }
+                return;
+            }
 
 
 
+            /*
+             * Method toString() - this is a helper method for printing / constructing
+             * a string object from the linked-list.
+             */
+            public String toString () // Method to output the LinkedList as a String
+            {
+                String output = "[";
+                Node currNode = this.head;
+                while (currNode != null) {
+                    output += currNode.data + " ";
+                    currNode = currNode.next;
+                }
+                return output.trim() + "]";
+            }
 
-    /*
-     * Class Stacks
-     *
-     * This class utilizes the Java Common Framework Library Stack class.
-     *
-     * The intent of this class is to write methods which utilize the Java
-     * library's Stack class. You need to utilize this library class in
-     * your solution.
-     *
-     * Methods:
-     *  - boolean isPalindrome(string)   - method returns true or false if 'string'
-     *                                     is a palindrome
-     *  - int     findlargestK(stack, k) - method accepts a stack and returns the
-     *                                     the largest index in the stack containing
-     *                                     value 'k' (stack index starts at 0)
-     *
-     */
+        } // End class LinkedList
 
-    static class Stacks {
+
+
 
         /*
-         * Method isPalindrome() - This method will return the boolean value 'true'
-         * or 'false' on if the passed in parameter string is a palindrome or not.
+         * Class Stacks
          *
-         * The routine should be case insensitive! Meaning 'RaCe cAr' is a palindrome.
-         * Moreover, spaces are ignore, so both 'race car' and 'racecar' are plaindromes.
+         * This class utilizes the Java Common Framework Library Stack class.
          *
-         * The method should utilize the provided Stack class.
+         * The intent of this class is to write methods which utilize the Java
+         * library's Stack class. You need to utilize this library class in
+         * your solution.
+         *
+         * Methods:
+         *  - boolean isPalindrome(string)   - method returns true or false if 'string'
+         *                                     is a palindrome
+         *  - int     findlargestK(stack, k) - method accepts a stack and returns the
+         *                                     the largest index in the stack containing
+         *                                     value 'k' (stack index starts at 0)
+         *
          */
-        public static boolean isPalindrome(String input) {
 
-            Stack<Character> stack = new Stack<>();
-            input = input.toLowerCase().replaceAll("\\s+", "");
+        static class Stacks {
 
-            // Your CODE GOES HERE
-            return false;
+            /*
+             * Method isPalindrome() - This method will return the boolean value 'true'
+             * or 'false' on if the passed in parameter string is a palindrome or not.
+             *
+             * The routine should be case insensitive! Meaning 'RaCe cAr' is a palindrome.
+             * Moreover, spaces are ignore, so both 'race car' and 'racecar' are plaindromes.
+             *
+             * The method should utilize the provided Stack class.
+             */
+            public static boolean isPalindrome(String input) {
+
+                Stack<Character> stack = new Stack<>();
+                input = input.toLowerCase().replaceAll("\\s+", "");
+
+                // push characters
+                for (int i = 0; i < input.length(); i++) {
+                    stack.push(input.charAt(i));
+                }
+
+               //poping characters
+
+                for (int i = 0; i < input.length(); i++) {
+                    if (input.charAt(i) != stack.pop()) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            /*
+             * Method findLargestk() - This method will return the largest index
+             * position in the stack for the value specified by the parameter 'k'.
+             *
+             * Note that the bottom of the stack is index location 0. So it you push
+             * on to the stack the values 3 4 9 4 4 7 4, in that order. And you pass the
+             * value '4' for the parameter k, then the largest index position is index
+             * location 6.
+             *
+             * The method should utilize the provided Stack class. This method should NOT
+             * destroy the passed in stack, meaning when the method returns, the passed in
+             * stack should be identical to when this method is passed. One trick as you
+             * pop elements off the passed in stack, place them in a temp stack. Then when
+             * completed, place them all back in teh original stack.
+             */
+            public static int findLargestK(Stack<Integer> stack, int k) {
+
+                // YOUR CODE GOES HERE
+                if (stack == null || stack.isEmpty()) return -1;
+
+                Stack<Integer> temp = new Stack<>();
+                int n = stack.size();
+                int popped = 0;
+                int result = -1;
+
+                while (!stack.isEmpty()) {
+                    int val = stack.pop();
+                    int indexFromBottom = n - 1 - popped; // bottom index 0
+                    if (val == k) {
+                        result = Math.max(result, indexFromBottom); // keep largest index
+                    }
+                    temp.push(val);
+                    popped++;
+                }
+
+                // Restore
+                while (!temp.isEmpty()) {
+                    stack.push(temp.pop());
+                }
+
+                if (result != -1) return result;
+
+                return -1;
+                }
+            } //End of class stack
+
+
+
+        /*******************************
+         *
+         * Algorithm Analysis
+         *
+         * Below are two methods, algorithmAnalysis1 and algorithmAnalysis2.
+         * Modify the return statement to return the correct option number.
+         *
+         *********************************/
+
+        public static int algorithmAnalysis1(int n, int m) {
+            int a = 0, b = 0;
+
+            for (int i=0; i < n; i++)
+                a+= Math.random();
+
+            for (int j=0; j < m; j++)
+                b+= Math.random();
+
+            /*
+             * Select the correct option listed below:
+             *   1. O(N * M) time, O(1) space
+             *   2. O(N + M) time, O(N + M) space
+             *   3. O(N + M) time, O(1) space
+             *   4. O(N * M) time, O(N + M) space
+             *
+             * TODO: return the answer (which option is correct), in the return statement
+             */
+
+            // RETURN THE CORRECT OPTION NUMBER LISTED ABOVE
+            return 3;
         }
 
 
-        /*
-         * Method findLargestk() - This method will return the largest index
-         * position in the stack for the value specified by the parameter 'k'.
-         *
-         * Note that the bottom of the stack is index location 0. So it you push
-         * on to the stack the values 3 4 9 4 4 7 4, in that order. And you pass the
-         * value '4' for the parameter k, then the largest index position is index
-         * location 6.
-         *
-         * The method should utilize the provided Stack class. This method should NOT
-         * destroy the passed in stack, meaning when the method returns, the passed in
-         * stack should be identical to when this method is passed. One trick as you
-         * pop elements off the passed in stack, place them in a temp stack. Then when
-         * completed, place them all back in teh original stack.
-         */
-        public static int findLargestK(Stack<Integer> stack, int k) {
+        public static int algorithmAnalysis2(int n) {
+            int i, j, k = 0;
+            for (i = n/2; i <= n; i++)
+                for ( j = 2; j <= n; j = j*2 )
+                    k+= n/2;
 
-            // YOUR CODE GOES HERE
-            return -1;
+            /*
+             * Select the correct option listed below:
+             *   1. O(N) time
+             *   2. O(N log N) time
+             *   3. O(N^2) time
+             *   4. O(N^2Log n) time
+             *
+             * TODO: return the answer (which option is correct), in the return statement
+             */
+
+            // RETURN THE CORRECT OPTION LISTED ABOVE
+            return 2;
         }
 
-    }  // End class Stacks
-
-
-
-    /*******************************
-     *
-     * Algorithm Analysis
-     *
-     * Below are two methods, algorithmAnalysis1 and algorithmAnalysis2.
-     * Modify the return statement to return the correct option number.
-     *
-     *********************************/
-
-    public static int algorithmAnalysis1(int n, int m) {
-        int a = 0, b = 0;
-
-        for (int i=0; i < n; i++)
-            a+= Math.random();
-
-        for (int j=0; j < m; j++)
-            b+= Math.random();
-
-        /*
-         * Select the correct option listed below:
-         *   1. O(N * M) time, O(1) space
-         *   2. O(N + M) time, O(N + M) space
-         *   3. O(N + M) time, O(1) space
-         *   4. O(N * M) time, O(N + M) space
-         *
-         * TODO: return the answer (which option is correct), in the return statement
-        */
-
-        // RETURN THE CORRECT OPTION NUMBER LISTED ABOVE
-        return -1;
     }
-
-
-    public static int algorithmAnalysis2(int n) {
-        int i, j, k = 0;
-        for (i = n/2; i <= n; i++)
-            for ( j = 2; j <= n; j = j*2 )
-                k+= n/2;
-
-        /*
-         * Select the correct option listed below:
-         *   1. O(N) time
-         *   2. O(N log N) time
-         *   3. O(N^2) time
-         *   4. O(N^2Log n) time
-         *
-         * TODO: return the answer (which option is correct), in the return statement
-         */
-
-        // RETURN THE CORRECT OPTION LISTED ABOVE
-        return -1;
-    }
-
-}
 
